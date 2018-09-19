5d81fe770ff5a342195a2f4f0ea4d7e42acf605c
furkantarikgocmen <f.tarikgocmen@outlook.com>
2018-09-20 01:20:18 +0300

    V2.0 User Selection, Message Counter

--- IG-DM-PARSER/Program.cs
+++ IG-DM-PARSER/Program.cs
@@ -51,36 +51,61 @@ namespace IG_DM_PARSER
                 Console.WriteLine("Copyright FTG Furkan Tarık Göçmen ©");
                 Console.WriteLine("Instagram @furkantarikgocmen");
                 Console.WriteLine("GitHub /Furkantarikgocmen");
-                Console.WriteLine("Put The File With The Application İn The Same Directory And Enter The File Name (Sample; messages.json)");
-                Console.WriteLine("Turkish Subtitle: Dosyayı Uygulama İle Aynı Dizine Koyunuz Ve Dosya İsmini Giriniz (Örnek; messages.json)");
-                string input = Console.ReadLine();
-                using (System.IO.StreamReader _StreamReader = new System.IO.StreamReader(input)) //Bin/Debug içerisindeki json dosyası
+                Console.WriteLine("Put The File (messages.json) With The Application İn The Same Directory");
+                Console.WriteLine("Turkish Subtitle: Dosyayı (messages.json) Uygulama İle Aynı Dizine Koyunuz");
+                Console.WriteLine("==!==Eğer sadece bir kullanıcının mesajlarını istiyor iseniz o kullanıcının ismini giriniz. Hepsini istiyorsanız kullanıcı adı kısmını boş bırakınız.==!==");
+                Console.WriteLine("==!==If you want only one user's message, please enter that user's name.If you want them all, leave the user name blank.=!==");
+                using (System.IO.StreamReader _StreamReader = new System.IO.StreamReader("messages.json"))
                 {
 
                     string jsonData = _StreamReader.ReadToEnd();
                     List<RootObject> Katilimcilar = Newtonsoft.Json.JsonConvert.DeserializeObject<List<RootObject>>(jsonData);
-                    // Deserialize edilecek olan class
+
+                    Console.WriteLine("Kullanıcı Adı Giriniz / Enter The Username");
+                    string kullanici = Console.ReadLine();
+
+                    int sayac = 0;
 
                     using (StreamWriter writer = new StreamWriter("output.txt"))
                     {
-                        writer.WriteLine("Transaction Is Starting");
+                        writer.WriteLine("=====Transaction Is Starting=====");
                         foreach (var _Mesaj in Katilimcilar)
                         {
-                            writer.WriteLine("KATILIMCILAR/PARTİCİPANTS:");
 
-                            foreach (var kullnicilar in _Mesaj.participants)
+                            foreach (var kullanicilar in _Mesaj.participants)
                             {
-                                writer.WriteLine(kullnicilar);
+
+
+                                if (kullanici == kullanicilar)
+                                {
+
+                                    writer.WriteLine("======MESAJLAR/MESSAGES=====");
+
+                                    foreach (var mesajlar in _Mesaj.conversation)
+                                    {
+                                        writer.WriteLine("{0}: {1} {2} {3} {4} {5} {6} {7} {8} {9} {10} {11} {12} ", mesajlar.sender, mesajlar.story_share, mesajlar.media_owner, mesajlar.media_share_caption, mesajlar.media_share_url, mesajlar.profile_share_username, mesajlar.profile_share_name, mesajlar.link, mesajlar.media, mesajlar.mentioned_username, mesajlar.text, mesajlar.heart, mesajlar.created_at);
+                                        Console.WriteLine("{0}: {1} {2} {3} {4} {5} {6} {7} {8} {9} {10} {11} {12} ", mesajlar.sender, mesajlar.story_share, mesajlar.media_owner, mesajlar.media_share_caption, mesajlar.media_share_url, mesajlar.profile_share_username, mesajlar.profile_share_name, mesajlar.link, mesajlar.media, mesajlar.mentioned_username, mesajlar.text, mesajlar.heart, mesajlar.created_at);
+                                        sayac++;
+                                    }
+                                }
+
+                                if (kullanici == string.Empty)
+                                {
+
+                                    writer.WriteLine("======MESAJLAR/MESSAGES=====");
+
+                                    foreach (var mesajlar in _Mesaj.conversation)
+                                    {
+                                        writer.WriteLine("{0}: {1} {2} {3} {4} {5} {6} {7} {8} {9} {10} {11} {12} ", mesajlar.sender, mesajlar.story_share, mesajlar.media_owner, mesajlar.media_share_caption, mesajlar.media_share_url, mesajlar.profile_share_username, mesajlar.profile_share_name, mesajlar.link, mesajlar.media, mesajlar.mentioned_username, mesajlar.text, mesajlar.heart, mesajlar.created_at);
+                                        Console.WriteLine("{0}: {1} {2} {3} {4} {5} {6} {7} {8} {9} {10} {11} {12} ", mesajlar.sender, mesajlar.story_share, mesajlar.media_owner, mesajlar.media_share_caption, mesajlar.media_share_url, mesajlar.profile_share_username, mesajlar.profile_share_name, mesajlar.link, mesajlar.media, mesajlar.mentioned_username, mesajlar.text, mesajlar.heart, mesajlar.created_at);
+
+                                        sayac++;
+                                    }
+                                }
                             }
 
-                            writer.WriteLine("MESAJLAR/MESSAGES:");
-
-                            foreach (var mesajlar in _Mesaj.conversation)
-                            {
-                                writer.WriteLine("{0}: {1} {2} {3} {4} {5} {6} {7} {8} {9} {10} {11} {12} ", mesajlar.sender, mesajlar.story_share, mesajlar.media_owner, mesajlar.media_share_caption, mesajlar.media_share_url, mesajlar.profile_share_username, mesajlar.profile_share_name, mesajlar.link, mesajlar.media, mesajlar.mentioned_username, mesajlar.text, mesajlar.heart, mesajlar.created_at);
-                                Console.WriteLine("{0}: {1} {2} {3} {4} {5} {6} {7} {8} {9} {10} {11} {12} ", mesajlar.sender, mesajlar.story_share, mesajlar.media_owner, mesajlar.media_share_caption, mesajlar.media_share_url, mesajlar.profile_share_username, mesajlar.profile_share_name, mesajlar.link, mesajlar.media, mesajlar.mentioned_username, mesajlar.text, mesajlar.heart, mesajlar.created_at);
-                            }
                         }
+                        Console.WriteLine("=====Toplam Mesaj Sayısı : {0}=====", sayac);
                     }
                 }
             }
